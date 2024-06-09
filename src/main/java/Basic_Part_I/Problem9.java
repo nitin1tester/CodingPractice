f770946f98be121c3e9d2c2864f391f2b81e84c4
Nitin Rastogi <24982227+nitin1tester@users.noreply.github.com>
2024-06-09 13:15:37 +0530

    rite a Java program to compute the specified expressions and print the output
    rite a Java program to compute the specified expressions and print the output

---
+++ src/main/java/Basic_Part_I/Problem9.java
@@ -0,0 +1,16 @@
+package Basic_Part_I;
+
+public class Problem9 {
+    public static void main(String[] args){
+        /*
+
+        9. Write a Java program to compute the specified expressions and print the output.
+        Test Data:
+        ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+        Expected Output
+        2.138888888888889
+        * */
+
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+}
