9e63ea12d5c0fc8ec28537334d8d8d26e28b6cb0
Nitin Rastogi <24982227+nitin1tester@users.noreply.github.com>
2024-06-09 13:45:35 +0530

    Write a Java program to compute a specified formula.
    Write a Java program to compute a specified formula.

---
+++ src/main/java/Basic_Part_I/Problem10.java
@@ -0,0 +1,19 @@
+package Basic_Part_I;
+
+public class Problem10 {
+    public static void main(String[] args){
+
+        /*
+        *
+        * Write a Java program to compute a specified formula.
+        Specified Formula :
+        4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+        Expected Output
+        2.9760461760461765
+        *
+        * */
+
+        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+
+    }
+}
