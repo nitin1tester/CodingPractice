fcf3c0926a7038916a46a51870f87f2df3b2a639
Nitin Rastogi <24982227+nitin1tester@users.noreply.github.com>
2024-06-09 09:13:00 +0530

    Write a Java program to divide two numbers and print them on the screen.
    Write a Java program to divide two numbers and print them on the screen.

---
+++ src/main/java/Basic_Part_I/Problem3.java
@@ -0,0 +1,16 @@
+package Basic_Part_I;
+
+public class Problem3 {
+    public static void main(String[] args){
+        /*Write a Java program to divide two numbers and print them on the screen.
+        Test Data :
+        50/3
+        Expected Output :
+        16
+        */
+        Integer firstNumber = 50;
+        Integer secondNumber = 3;
+        Integer result = firstNumber/secondNumber;
+        System.out.println(result);
+    }
+}
