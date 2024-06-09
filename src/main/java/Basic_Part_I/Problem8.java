4bebc96f064ac1b6b0e7c37e324df425f60e8e74
Nitin Rastogi <24982227+nitin1tester@users.noreply.github.com>
2024-06-09 13:05:52 +0530

    Write a Java program to display the following pattern.
    Write a Java program to display the following pattern.

---
+++ src/main/java/Basic_Part_I/Problem8.java
@@ -0,0 +1,23 @@
+package Basic_Part_I;
+
+public class Problem8 {
+    public static void main(String[] args){
+        /*
+        *
+        * Write a Java program to display the following pattern.
+            Sample Pattern :
+
+           J    a   v     v  a
+           J   a a   v   v  a a
+      J    J  aaaaa   V V  aaaaa
+        JJ   a     a   V  a     a
+        *
+        * */
+
+        System.out.println("     J    a   v     v  a      ");
+        System.out.println("     J   a a   v   v  a a     ");
+        System.out.println("J    J  aaaaa   V V  aaaaa    ");
+        System.out.println("  JJ   a     a   V  a     a   ");
+
+    }
+}
