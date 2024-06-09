688f6b11821486d920736f8ab74e34efa4ab21b4
Nitin Rastogi <24982227+nitin1tester@users.noreply.github.com>
2024-06-09 10:34:48 +0530

    Write a Java program that takes two numbers as input and displays the product of two numbers.
    Write a Java program that takes two numbers as input and displays the product of two numbers.

---
+++ src/main/java/Basic_Part_I/Problem5.java
@@ -0,0 +1,20 @@
+package Basic_Part_I;
+
+public class Problem5 {
+
+    public static void main(String[] args){
+        /*
+        * 5. Write a Java program that takes two numbers as input and displays the product of two numbers.
+        Test Data:
+        Input first number: 25
+        Input second number: 5
+        Expected Output :
+        25 x 5 = 125
+        * */
+
+        Integer first_number = 25;
+        Integer second_number = 5;
+        Integer result = first_number*second_number;
+        System.out.println(result);
+    }
+}
