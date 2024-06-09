4d0c0fbb467fe61f24598dc4c4aeb80c0d32e25a
Nitin Rastogi <24982227+nitin1tester@users.noreply.github.com>
2024-06-09 09:06:07 +0530

    Write a Java program to print the sum of two numbers.
    Write a Java program to print the sum of two numbers.

--- src/main/java/Basic_Part_I/Problem1.java
+++ src/main/java/Basic_Part_I/Problem1.java
@@ -1,6 +1,11 @@
 package Basic_Part_I;
 
 public class Problem1 {
+    /*Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+Nitin Rastogi*/
+
     public static void main(String[] args){
         System.out.print("Hello " + "\nNitin Rastogi" );
     }
---
+++ src/main/java/Basic_Part_I/Problem2.java
@@ -0,0 +1,17 @@
+package Basic_Part_I;
+
+public class Problem2 {
+    public static void main(String[] args){
+        /*2. Write a Java program to print the sum of two numbers.
+            Test Data:
+            74 + 36
+            Expected Output :
+            110*/
+
+        Integer a = 74;
+        Integer b = 36;
+        Integer total = a+b;
+        System.out.println(total);
+
+    }
+}
