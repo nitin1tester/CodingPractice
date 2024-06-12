9a94684f0969bb5ec1e2dbc03c0592bbf553fdc2
Nitin Rastogi <24982227+nitin1tester@users.noreply.github.com>
2024-06-12 06:50:15 +0530

    //55. Jump Game updated
    //55. Jump Game updated

--- src/main/java/LeetCode/Jump.java
+++ src/main/java/LeetCode/Jump.java
@@ -2,15 +2,12 @@ package LeetCode;
 //55. Jump Game
 public class Jump {
     public static void main(String[] args){
-        int[] nums = {2,1,3,2,5,2,3,3,3,2,1};
+        int[] nums = {2,0};
         for (int i = 0; i <= nums.length-1; i=i+nums[i]) {
             if (i == nums.length-1) {
                 System.out.println("True");
                 break;
-            } else if (i > nums.length-1) {
-                System.out.println("False");
-                break;
-            } else if (nums[i]==0) {
+            } else if (i >= nums.length || nums[i]==0 || nums[0]> nums.length-1 ) {
                 System.out.println("False");
                 break;
             }
